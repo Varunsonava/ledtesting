63a2583eb3d55c47027558a751b9ddb3a047ae2f
Varun <varunsonava@gmail.com>
2018-08-06 01:12:52 +0530

    user n password changed

--- app.js
+++ app.js
@@ -369,9 +369,9 @@ class FacebookBot {
 
         var options = {
             clientId: 'mqttjs_' + Math.random().toString(16).substr(2, 8),
-            username: 'user1',
+            username: 'xswfiggi',
             //username: 'xswfiggi', 
-            password: 'password'
+            password: '8JBNzvH5fmG0'
             //password: '8JBNzvH5fmG0'
         };
 
